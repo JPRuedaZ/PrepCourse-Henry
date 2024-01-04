1767dfb2666e990e2b8049b373a0346e90f7f887
masterprogram10 <masterprogam10@gmail.com>
2024-01-04 10:37:21 -0500

    avance a mod 7

---
+++ homeworks/js/variables .js
@@ -0,0 +1,20 @@
+// String
+let bootcamp = 'Henry';
+let tamano = bootcamp.length;
+console.log(tamano);
+
+// Números
+let numeroEntero = 17;
+let numeroNegativo = -4;
+let numeroDecimal = 11.75;
+
+// Booleanos
+let estoyEntendiendo = true;
+let estoyAburrido = false;
+
+// Undefined
+let cajon1;
+console.log(cajon1);
+
+// Null
+let sinNada = null;
